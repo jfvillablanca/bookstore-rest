f079a90edae0737aef0ae4c3a40bc25b9a7b79d1
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-10-03 19:25:06 +0800

    setup typeorm module at app level

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRootAsync({
+            useFactory: () => ({
+                type: 'mysql',
+                host: process.env.DB_HOST || 'localhost',
+                port: 3306,
+                username: process.env.DB_USERNAME || 'root',
+                password: process.env.DB_PASSWORD || 'rootpassword',
+                database: process.env.DB_DATABASE || 'bookstore_rest',
+                entities: [],
+                synchronize: true,
+            }),
+        }),
     controllers: [AppController],
     providers: [AppService],
 })
